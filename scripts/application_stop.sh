47ddb82dd81eca6c3598a33f7a297337e2b47d0b
vishal.vashisht1 <vishal.vashisht@quovantis.com>
2021-04-28 11:17:53 +0530

    built sample app

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers"
-pid=lsof -t -i:$server_port
-if [ "$pid" != "" ]
-then # Kill the running process
-kill -9 $pid 2>/dev/null || :
-fi
+pkill node
+echo "Stopped all existing node servers"
